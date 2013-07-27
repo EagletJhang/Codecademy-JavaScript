32aefe718e43f65879bb3bcebc4e338b2f47f684
Eagelt Jhang <eagletblueblue@gmail.com>
2013-07-27 18:53:51 +0800

    Create Playing with strings and numbers.js

---
+++ Getting-Started-with-Programming/Playing with strings and numbers.js
@@ -0,0 +1,10 @@
+/*
+( ): control order of operations
+2. * and /: multiplication and division
+3. - and +: subtraction and addition
+*/
+//9模3
+9 % 3
+
+"some word".substring(x, y) 
+//從0開始編號, x是第一個包括的字母的序號, y是第一個不包括的字母的序號.
