b8a807f205c8bd02ac671799f76efb7b8d60646a
Eagelt Jhang <eagletblueblue@gmail.com>
2013-07-27 17:24:01 +0800

    Create Make the computer think.js

---
+++ Getting-Started-with-Programming/Make the computer think.js
@@ -0,0 +1,21 @@
+console.log(2 * 5)
+console.log("Hello")
+//把括号内的字符串输出到控制台
+
+/*
+> Greater than
+< Less than
+<= Less than or equal to
+>= Greater than or equal to
+=== Equal to
+!== Not equal to
+*/
+
+if (//this condition is true) 
+{
+    // do this code
+}
+else // "otherwise"
+{
+    // do this code instead
+}
