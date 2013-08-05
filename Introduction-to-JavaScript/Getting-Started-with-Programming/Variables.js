22a501491035c2cf4909e2c7a785770c1a3b7e9a
Eagelt Jhang <eagletblueblue@gmail.com>
2013-08-05 18:45:35 +0800

    Create Variables.js

---
+++ Introduction-to-JavaScript/Getting-Started-with-Programming/Variables.js
@@ -0,0 +1,6 @@
+// myCountry and give it a string value.
+myCountry="China";
+// Use console.log to print out the length of the variable myCountry.
+console.log( myCountry.length);
+// Use console.log to print out the first three letters of myCountry.
+console.log(myCountry.substring(0,3) );
