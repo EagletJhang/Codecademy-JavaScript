45d4e3e936117882946aee421ff8586146e9d871
Eagelt Jhang <eagletblueblue@gmail.com>
2013-08-05 19:06:36 +0800

    Create Project: Choose Your Own Adventure.js

---
+++ Introduction-to-JavaScript/Project: Choose Your Own Adventure.js
@@ -0,0 +1,18 @@
+// Check if the user is ready to play!
+confirm("I am ready to play!");
+var age = prompt("how old are you?");
+if(age < 18){
+    console.log("play but you take no responsibility")
+}
+else{
+    console.log(" play on!")
+}
+console.log("Snow White and Batman were hanging out at the bus stop, waiting to go to the shops. There was a sale on and both needed some new threads. You've never really liked Batman. You walk up to him.")
+console.log("Batman glares at you.")
+var userAnswer = prompt("Are you feeling lucky, punk?")
+if (userAnswer == "yes"){
+    console.log("Batman hits you very hard. It's Batman and you're you! Of course Batman wins!")
+}
+else{
+    console.log("You did not say yes to feeling lucky. Good choice! You are a winner in the game of not getting beaten up by Batman.")
+}
