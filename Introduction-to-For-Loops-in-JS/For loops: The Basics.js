db8a5814d77996d94133cffbebd0053a89de266a
Eagelt Jhang <eagletblueblue@gmail.com>
2013-08-06 20:16:14 +0800

    Create For loops: The Basics.js

---
+++ Introduction-to-For-Loops-in-JS/For loops: The Basics.js
@@ -0,0 +1,9 @@
+for (var i = 1; i < 11; i = i + 1) {
+  code code code; 
+  }
+  //i=i+1可写成i++, 同理i--.
+  
+for (var i = 5; i < 51; i+=5) {
+  console.log(i);
+  }
+  //i += x; x表示增量的步长. 同理, i -= x;
