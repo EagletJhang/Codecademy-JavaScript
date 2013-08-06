336e0bf6afb84d4c22c698a5e17f6086528d3903
Eagelt Jhang <eagletblueblue@gmail.com>
2013-08-06 20:02:58 +0800

    Create Project: Review of Functions in JavaScript.js

---
+++ Functions/Project: Review of Functions in JavaScript.js
@@ -0,0 +1,17 @@
+var cube = function (x) {
+    return x * x *x;
+};
+
+cube(7);
+console.log(cube("test")); 
+//输出NaN not a number
+
+//定义体积函数, 三个变量
+var volume = function (w, l, h ) {
+    if (typeof(x) !== 'number') return 0;
+    //显示输入的数据类型
+    return w * l * h;
+};
+
+volume(2, 3, 4);
+
