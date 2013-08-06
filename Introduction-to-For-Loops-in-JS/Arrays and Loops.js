80df265020678a7528bfa1e1a6a6ffb672a2eb81
Eagelt Jhang <eagletblueblue@gmail.com>
2013-08-06 21:09:44 +0800

    Create Arrays and Loops.js

---
+++ Introduction-to-For-Loops-in-JS/Arrays and Loops.js
@@ -0,0 +1,18 @@
+//First element in the array: junkData[0]
+//Third element in the array: junkData[2]
+
+var cities = ["Melbourne", "Amman", "Helsinki", "London"];
+
+for (var i = 0; i < cities.length; i++) {
+    console.log("I would like to visit " + cities[i]);
+}
+
+//找出array中的最大数
+var array = [3, 6, 2, 56, 32, 5, 89, 32];
+var largest = 0;
+for (i=0;i<8;i++){
+    if(largest<array[i]){
+            largest = array[i];
+    }
+}
+console.log(largest);
