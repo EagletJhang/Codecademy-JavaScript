c2a575670530468eff649d5e030229e504d822b0
Eagelt Jhang <eagletblueblue@gmail.com>
2013-08-06 20:05:52 +0800

    Update Project: Review of Functions in JavaScript.js

--- Functions/Project: Review of Functions in JavaScript.js
+++ Functions/Project: Review of Functions in JavaScript.js
@@ -12,6 +12,17 @@ var volume = function (w, l, h ) {
     //显示输入的数据类型
     return w * l * h;
 };
-
 volume(2, 3, 4);
 
+
+
+var area = 36;
+var volume = function (w, l, h) {
+    var area = w * l;
+    //局部变量
+    return area * h;
+};
+
+console.log(volume(2, 3, 4));
+console.log(area);
+//调用全局变量, 输出36. 若21行没有var, 则变成新的赋值, 这里输出的是更新后值为6.
