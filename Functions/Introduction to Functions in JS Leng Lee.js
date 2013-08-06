f3d2f1d4cc0386045f79e1ce4e7b0a2a29fa4555
Eagelt Jhang <eagletblueblue@gmail.com>
2013-08-06 16:50:05 +0800

    Create Introduction to Functions in JS Leng Lee.js

---
+++ Functions/Introduction to Functions in JS Leng Lee.js
@@ -0,0 +1,17 @@
+var balance = 20.97;
+
+// Complete the condition in the ()s on line 4
+if (balance < 10) {
+  // console.log() the balance minus 5 dollars
+	console.log(balance-5)
+} else {
+    console.log(balance)
+	// Just console.log() the balance
+}
+
+
+//function
+var greeting = function (name) {
+    console.log("Great to see you," + " " + name);
+};
+greeting("Kim")
