9fe96d649c20a316812eb3fa1dd4a329ad8ad214
Eagelt Jhang <eagletblueblue@gmail.com>
2013-08-14 20:43:23 +0800

    Create Review: The story so far.js

---
+++ Objects-I/Introduction-to-Objects-I/Review: The story so far.js
@@ -0,0 +1,22 @@
+// Here is an array of multiples of 8. But is it correct?
+var multiplesOfEight = [8,16,24,32,40,58];
+
+// Test to see if a number from the array is NOT a true
+// multiple of eight. Real multiples will return false.
+var answer = multiplesOfEight[5] % 8 !== 0;
+
+//算法
+for(i = 1;i < 21;i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    }
+    else if(i % 3 === 0){
+        console.log("Fizz");
+    }
+    else if(i % 5 === 0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
